d4bcf0267718ee448cf9d428789312f269a830d5
Gioele Muscolino <gioele.muscolino@gmail.com>
2025-04-08 17:24:25 +0200

    last changes

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+//import ExifReader from 'exifreader';
+
 var theUrl = "https://tourism.api.opendatahub.com/v1/WebcamInfo?pagenumber=1&pagesize=20&active=true&fields=Shortname&fields=ImageGallery&removenullvalues=true&getasidarray=false";
+var dateUrl = "https://tourism.api.opendatahub.com/v1/WebcamInfo?pagenumber=1&pagesize=20&active=true&fields=Shortname&fields=ImageGallery&removenullvalues=true&getasidarray=false";
 
     var currentPage = 1;
     var table = document.getElementById("table");
@@ -28,10 +31,12 @@ function fillTable (testo) {
     // Insert a new cell (<td>) at the first position of the "new" <tr> element:
     var cell = row.insertCell(0);
     var cell1 = row.insertCell(1);
+    var cell2 = row.insertCell(2);
 
     // Add some bold text in the new cell:
     cell.innerHTML = "<b>Name</b>";
     cell1.innerHTML = "<b>Image</b>";
+    cell2.innerHTML = "<b>Status</b>";
 
     for (let i = 0; i<obj.Items.length; i++){
 
@@ -41,12 +46,23 @@ function fillTable (testo) {
         // Insert new cells (<td> elements) at the 1st and 2nd position of the "new" <tr> element:
         var cell1 = row.insertCell(0);
         var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+
+        var imgUrl = obj.Items[i].ImageGallery[0].ImageUrl;
+        var imgId = "imgId" + i;
+        var img = "<img id='"+ imgId +"' width = 200px src='" + imgUrl +"'/>";
+
+
+        var imgInGallery = obj.Items[i].ImageGallery[0].IsInGallery;
 
         // Add some text to the new cells:
-
         cell1.innerHTML = obj.Items[i].Shortname;
-        cell2.innerHTML = "<img id='provaImmagine' width = 200px src='" + obj.Items[i].ImageGallery[0].ImageUrl +"'/>" ;
-
+        cell2.innerHTML = img;
+        if(imgInGallery) {
+            cell3.innerHTML = "ok";
+        } else {
+            cell3.innerHTML = "error";
+        }
     }
 }
 
@@ -69,4 +85,19 @@ function onClickChangeWebcamPage(direction){
     xmlHttp.send(null);
     
     fillTable();
+}
+
+function getExif(imgId, cell) {
+    var img1 = document.getElementById(imgId);
+    EXIF.getData(img1, function() {
+        /*
+        var date = EXIF.getTag(this, "ModifyDate");
+        cell.innerHTML = `${date}`;
+        */
+        var make = EXIF.getTag(this, "Make");
+        var model = EXIF.getTag(this, "Model");
+        var makeAndModel = document.getElementById("makeAndModel");
+        cell.innerHTML = `${make} ${model}`;
+        
+    });
 }
